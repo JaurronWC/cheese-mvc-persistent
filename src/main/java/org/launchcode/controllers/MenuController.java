059975706903ecd0431654948f2f481801bcc901
Jaurron Crawford <jaurroncrawford@yahoo.com>
2017-06-22 20:20:10 -0500

    Completed second part of assignment, began work on third

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,32 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Jaurron on 6/22/2017.
+ */
+@Controller
+@RequestMapping(value = "menu")
+public class MenuController {
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @RequestMapping(value = "")
+    private String index(Model model){
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "Menus");
+
+        return "/menu";
+
+    }
+}
